361ab9b4ca6762a2a273d5a365f0e864a081b76c
EliasYoc <eliyoc_97@hotmail.com>
2025-03-01 22:25:46 -0600

    fix: type params

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default async function SelectedMovie({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
   const movieRes = await getMovieById(id);
