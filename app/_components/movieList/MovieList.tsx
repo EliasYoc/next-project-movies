f24beadb737cd4a32869c99ca239f931d1127586
EliasYoc <eliyoc_97@hotmail.com>
2025-03-14 23:50:33 -0600

    fix:warnings

--- app/_components/movieList/MovieList.tsx
+++ app/_components/movieList/MovieList.tsx
@@ -1,5 +1,5 @@
 // https://developer.themoviedb.org/reference/discover-movie
-import Poster from "../ui/Poster";
+
 import { getTmdbConfiguration } from "../../_services/tmdb/configuration";
 import { getMovieList } from "../../_services/tmdb/discover";
 import List from "@/app/movie/_components/movieRecommendations/List";
@@ -19,20 +19,6 @@ export default async function MovieList({
     <section className={`movie-list ${className}`}>
       {title && <h2 className="text-3xl font-semibold">{title}</h2>}
       <List movieData={movieData} tmdbDetailsData={tmdbDetailsData} />
-      {/* <div className="scrollbar-hidden flex overflow-x-scroll gap-2">
-        {movieData.results.map((movie) => (
-          <Poster
-            navigateTo={`/movie/${movie.id}`}
-            key={movie.id}
-            title={!movie.poster_path ? movie.title : null}
-            src={
-              !movie.poster_path
-                ? "/assets/image-not-available.jpg"
-                : `${tmdbDetailsData.images.secure_base_url}${tmdbDetailsData.images.poster_sizes[2]}${movie.poster_path}`
-            }
-          />
-        ))}
-      </div> */}
     </section>
   );
 }
